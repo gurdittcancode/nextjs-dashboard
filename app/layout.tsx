3d854422ac22c594ba4bac6c17a426c089ce26b3
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2024-02-19 19:27:30 +0530

    Finished app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    //to minimize writing | AD everywhere, %s acts as a wildcard which can be anything
+    default: 'Acme Dashboard'
+  },
+  description: 'The official Next.js Course dashboard, built with App router',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
